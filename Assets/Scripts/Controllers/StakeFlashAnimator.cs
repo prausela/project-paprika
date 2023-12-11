bc560dffdb8046328d0091168e1a427c7a921957
Santiago Terenziani <sterenziani@itba.edu.ar>
2023-12-11 12:04:21 -0300

    Fix: Made stake flash animation visible in builds

--- Assets/Scripts/Controllers/StakeFlashAnimator.cs
+++ Assets/Scripts/Controllers/StakeFlashAnimator.cs
@@ -6,7 +6,8 @@ public class StakeFlashAnimator : MonoBehaviour
 {
     public float loopDuration = 0.3f;
     private Vector3 peakScale;
-    private const float growAnimationPercentage = 0.025f;
+    private const float growAnimationPercentage = 0.1f;
+    private float lastCallTime;
 
     void Start()
     {
@@ -15,19 +16,26 @@ public class StakeFlashAnimator : MonoBehaviour
     }
 
     public void Flash(){
-        StartCoroutine(AnimateFlash());
+        lastCallTime = Time.time;
+        StartCoroutine(AnimateFlash(lastCallTime));
     }
 
-    IEnumerator AnimateFlash(){
-        yield return StartCoroutine(Resize(peakScale, loopDuration*growAnimationPercentage));
-        yield return StartCoroutine(Resize(new Vector3(0,0,0), loopDuration*(1-growAnimationPercentage)));
+    IEnumerator AnimateFlash(float triggerTime){
+        if(triggerTime == lastCallTime)
+            yield return StartCoroutine(Resize(peakScale, loopDuration*growAnimationPercentage, triggerTime));
+        if(triggerTime == lastCallTime)
+            yield return StartCoroutine(Resize(new Vector3(0,0,0), loopDuration*(1-growAnimationPercentage), triggerTime));
     }
 
-    IEnumerator Resize(Vector3 targetScale, float time){
+    IEnumerator Resize(Vector3 targetScale, float time, float triggerTime){
         Vector3 originalScale = transform.localScale;
         for(float t = 0; t < 1; t += Time.deltaTime / time){
-            transform.localScale = Vector3.Lerp(originalScale, targetScale, t);
-            yield return null; // Wait until next frame
+            if(triggerTime != lastCallTime){
+                t = 1;  // Skip to end of animation
+            } else{
+                transform.localScale = Vector3.Lerp(originalScale, targetScale, t);
+                yield return null; // Wait until next frame
+            }
         }
     }
 }
